6b02afb56377f17f2433e60484eb35b72d01657b
Shayan Sen <senshayan2103@gmail.com>
2025-04-04 11:44:57 +0530

    smsmsms

--- src/test/java/com/shayan/ShayanSchool/ShayanSchoolApplicationTests.java
+++ src/test/java/com/shayan/ShayanSchool/ShayanSchoolApplicationTests.java
@@ -1,10 +1,10 @@
-package com.shayan.ShayanTest;
+package com.shayan.ShayanSchool;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShayanTestApplicationTests {
+class ShayanSchoolApplicationTests {
 
 	@Test
 	void contextLoads() {
